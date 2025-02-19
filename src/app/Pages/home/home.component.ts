c0864fb965ee46bfb48b01056a42fe64770ef789
larialvesg <larissaalvesgeraldoo@gmail.com>
2025-02-19 16:17:09 -0300

    19/02/2025 16:17

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
+  produtos = signal([
+    {
+      nome: "Uma vida pequena",
+      editora: "Editora Record",
+      preco: "R$76,12",
+      img: "https://m.media-amazon.com/images/I/41B6dmnbL7S._SY445_SX342_.jpg"
+    },
+    {
+      nome: "Três",
+      editora: "Editora Intrínseca",
+      preco: "R$48,81",
+      img: "https://m.media-amazon.com/images/I/51QSDfAlINL._SY445_SX342_.jpg"
+    },
+    {
+      nome: "Jogos Vorazes",
+      editora: "Editora Rocco",
+      preco: "R$40,25",
+      img: "https://m.media-amazon.com/images/I/41pbZcXFYkL._SY445_SX342_.jpg"
+    },
+
+  ])
 
 }
