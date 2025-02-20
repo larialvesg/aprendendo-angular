b5a4e606ef7e9eb8d7ec28e3d51feafb9f9a0acc
larialvesg <larissaalvesgeraldoo@gmail.com>
2025-02-20 13:39:24 -0300

    20/02/2025

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -10,22 +11,22 @@ export class HomeComponent {
   
   produtos = signal([
     {
-      nome: "Uma vida pequena",
-      editora: "Editora Record",
-      preco: "R$76,12",
-      img: "https://m.media-amazon.com/images/I/41B6dmnbL7S._SY445_SX342_.jpg"
+      nome: "Body Preto - Matilda",
+      avaliacoes: "★★★★★ (164)",
+      preco: "R$99,95",
+      img: "https://acdn-us.mitiendanube.com/stores/001/896/879/products/pin-1-46-f6a1572516eecb813b17097635803691-1024-1024.webp"
     },
     {
-      nome: "Três",
-      editora: "Editora Intrínseca",
-      preco: "R$48,81",
-      img: "https://m.media-amazon.com/images/I/51QSDfAlINL._SY445_SX342_.jpg"
+      nome: "Body Preto - Margot",
+      avaliacoes: "★★★★★ (20)",
+      preco: "R$99,95",
+      img: "https://acdn-us.mitiendanube.com/stores/001/896/879/products/vest-32-ab6193a10e7e46a5a317295219066460-1024-1024.webp"
     },
     {
-      nome: "Jogos Vorazes",
-      editora: "Editora Rocco",
-      preco: "R$40,25",
-      img: "https://m.media-amazon.com/images/I/41pbZcXFYkL._SY445_SX342_.jpg"
+      nome: "Body Preto - Charly",
+      avaliacoes: "★★★★★ (11)",
+      preco: "R$99,95",
+      img: "https://acdn-us.mitiendanube.com/stores/001/896/879/products/body-1-2f3130130c4e1f435717248513380141-1024-1024.webp"
     },
 
   ])
